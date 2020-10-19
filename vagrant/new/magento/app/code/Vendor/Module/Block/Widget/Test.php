278e505cf744aac5bbec36db2c31ddca17855c52
Sergey Sivkov <sergey.sivkov@onilab.com>
2020-10-19 19:17:08 +0300

    try add product in widget

--- vagrant/new/magento/app/code/Vendor/Module/Block/Widget/Test.php
+++ vagrant/new/magento/app/code/Vendor/Module/Block/Widget/Test.php
@@ -2,10 +2,10 @@
 
 namespace Vendor\Module\Block\Widget;
 
-use Magento\Framework\View\Element\Template;
+use Magento\CatalogWidget\Block\Product\Widget\Conditions;
 use Magento\Widget\Block\BlockInterface;
 
-class Test extends Template implements BlockInterface
+class Test extends Conditions implements BlockInterface
 {
     protected $_template = "widget/test.phtml";
 }
